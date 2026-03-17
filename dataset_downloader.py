b5f0e68d966cb19bf6149e28fbb384f353ed399b
Jashan Gupta <jashan0154@gmail.com>
2026-03-17 13:42:13 +0000

    changed dataset_downloader

--- dataset_downloader.py
+++ dataset_downloader.py
@@ -1,5 +1,10 @@
 from datasets import load_dataset
 
-dataset = load_dataset("shunk031/MSCOCO")
+dataset = load_dataset(
+    "shunk031/MSCOCO",
+    year=2014,
+    coco_task="captions"
+)
 
 print(dataset)
+print(dataset["train"][0])
